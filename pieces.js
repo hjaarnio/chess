62cba1973aeadaf170cfa6b64047aa70b618b690
hjaarnio <hjaarnio@gmail.com>
2013-09-16 21:33:23 +0300

    Added moving logic for rooks

--- pieces.js
+++ pieces.js
@@ -14,6 +14,10 @@ function Piece(type, side){
 	}
 }
 
+function containsOwn(x2, y2, side){
+	return grid.squares[y2][x2] != null && grid.squares[y2][x2].side == side;
+}
+
 function Pawn(side){
 	Piece.call(this, "P", side);
 	
@@ -45,8 +49,44 @@ function Rook(side){
 	Piece.call(this, "R", side);
 	
 	this.legalMove = function(x1, y1, x2, y2) {
-		
-		return true;
+		if(containsOwn(x2, y2, this.side)){
+			return false;
+		}
+		if(x1 != x2 && y1 != y2){ //move must be exactly horizontal or vertical
+			return false;
+		}
+		if(x1 == x2){
+			if(y1 > y2){
+				for(var i = y1 - 1; i > y2; i--){
+					if(grid.squares[i][x1] != null){
+						return false;
+					}
+				}
+			} else {
+				for(var i = y1 + 1; i < y2; i++){
+					if(grid.squares[i][x1] != null){
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+		if(y1 == y2){
+			if(x1 > x2){
+				for(var i = x1 - 1; i > x2; i--){
+					if(grid.squares[y1][i] != null){
+						return false;
+					}
+				}
+			} else {
+				for(var i = x1 + 1; i < x2; i++){
+					if(grid.squares[y1][i] != null){
+						return false;
+					}
+				}
+			}
+			return true;
+		}
 	}
 }
 Rook.prototype = new Piece;
