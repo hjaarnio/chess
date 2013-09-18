d834505aed5d70b125d9a3a86edeae75d10efd9e
hjaarnio <hjaarnio@gmail.com>
2013-09-18 19:34:31 +0300

    implemented a copy method for grid, further groundwork for king move check

--- logic.js
+++ logic.js
@@ -22,6 +22,25 @@ function Grid(){
 	
 	this.squares[7] = [new Rook(2), new Knight(2), new Bishop(2), new Queen(2), new King(2), new Bishop(2), new Knight(2), new Rook(2)];
 	this.squares[6] = [new Pawn(2), new Pawn(2), new Pawn(2), new Pawn(2), new Pawn(2), new Pawn(2), new Pawn(2), new Pawn(2)];
+	
+	this.copy = function(){ //returns a copy of the grid
+		var result = new Grid();
+		for(i = 0; i < this.gridWidth; i++){
+			for(j = 0; j < this.gridHeight; j++){
+				if(this.squares[i][j] == null){
+					result.squares[i][j] = null;
+				}else switch(this.squares[i][j].type){					
+					case "P": result.squares[i][j] = new Pawn(this.squares[i][j].side); break;
+					case "R": result.squares[i][j] = new Rook(this.squares[i][j].side); break;
+					case "N": result.squares[i][j] = new Knight(this.squares[i][j].side); break;
+					case "B": result.squares[i][j] = new Bishop(this.squares[i][j].side); break;
+					case "Q": result.squares[i][j] = new Queen(this.squares[i][j].side); break;
+					case "K": result.squares[i][j] = new King(this.squares[i][j].side); break;
+				}
+			}
+		}
+		return result;
+	}
 }
 
 function move(x1, y1, x2, y2, grid){
@@ -56,5 +75,4 @@ function checkValidMove(x1, y1, x2, y2, grid){
 		alert("wrong turn (or no piece)")
 		return false;
 	}
-	
 }
