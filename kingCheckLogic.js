69e7afad474f9bc718884d29ed0d237b37da7211
hjaarnio <hjaarnio@gmail.com>
2013-09-20 20:30:23 +0300

    Wrote isKingInCheck using movesets from the pieces, also isSquareInCheck for checking empty squares for castling

--- kingCheckLogic.js
+++ kingCheckLogic.js
@@ -1,40 +1,46 @@
 //checks if king is in check position in the current configuration of the field.
 //used to check if move is legal after move is executed on a copy of the field,
 //and in the end of turn to see if the move checked the opposing king
-function isKingInCheck(side, grid){ 
-		var x, y; //location of king
-		for(i = 0; i < grid.gridWidth; i++){
-			for(j = 0; j < grid.gridHeight; j++){
-				if(grid.squares[i][j] != null && grid.squares[i][j].side == side && grid.squares[i][j].type == "K"){
-					x = j; y = i;
-					break;
-				}
+function isKingInCheck(side, grid){
+	var x, y; //location of king
+	for(i = 0; i < grid.gridWidth; i++){
+		for(j = 0; j < grid.gridHeight; j++){
+			if(grid.squares[i][j].piece != null && grid.squares[i][j].piece.side == side && grid.squares[i][j].piece.type == "K"){
+				x = j; y = i;
+				break;
 			}
 		}
-		return !(verifyPawns(x, y, side, grid) && verifyKnights(x, y, side, grid) && verifyRooks(x, y, side, grid)
-			 && verifyBishops(x, y, side, grid) && verifyKings(x, y, side, grid));
+	}
+	alert("king at " + x + " " + y)
+	return isSquareInCheck(x, y, side, grid);
 }
 
-function verifyPawns(x, y, side, grid){
-	if((grid.squares[y + (side * -2 + 3)][x + 1] != null && grid.squares[y + (side * -2 + 3)][x + 1].type == "P" && grid.squares[y + (side * -2 + 3)][x + 1].side != side) ||
-	   (grid.squares[y + (side * -2 + 3)][x - 1] != null && grid.squares[y + (side * -2 + 3)][x - 1].type == "P" && grid.squares[y + (side * -2 + 3)][x - 1].side != side)){
-			return false;
-	   }
-	return true;
-}
-
-function verifyKnights(x, y, side, grid){
-	return true;
-}
-
-function verifyRooks(x, y, side, grid){ //contains also queen
-	return true;
-}
-
-function verifyBishops(x, y, side, grid){ //contains also queen
-	return true;
-}
-
-function verifyKings(x, y, side, grid){
-	return true;
+function isSquareInCheck(x, y, side, grid){
+	var emptySquare;
+	var inDangerSet = new Array;
+	
+	alert("enter isSquareInCheck")
+	
+	if(grid.squares[y][x].piece == null){
+		emptySquare = true;
+		grid.squares[y][x].piece = new Piece("X", side);
+	}
+	for(i = 0; i < grid.gridWidth; i++){
+		for(j = 0; j < grid.gridHeight; j++){
+			if(grid.squares[j][i].piece != null && grid.squares[j][i].piece.side != side){
+		//		alert("found " + grid.squares[j][i].piece.type)
+				inDangerSet = inDangerSet.concat(grid.squares[j][i].piece.moveset(i, j, grid));
+			}
+		}
+	}
+	
+	if(emptySquare){
+		grid.squares[y][x].piece = null;
+	}
+	
+	if(inDangerSet.indexOf(grid.squares[y][x]) == -1){
+		return false;
+	} else {
+		return true;
+	}
 }
