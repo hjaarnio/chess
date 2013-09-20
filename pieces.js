024d5e0adc765ab4ac4cc932dbe9c259bf48fcd7
hjaarnio <hjaarnio@gmail.com>
2013-09-20 17:22:55 +0300

    Refactored queen move code to use a moveset function using an array

--- pieces.js
+++ pieces.js
@@ -171,77 +171,88 @@ King.prototype = new Piece;
 function Queen(side){
 	Piece.call(this, "Q", side);
 	
+	this.moveset = function(x1, y1, grid){
+		alert("enter moveset")
+		moveset = new Array();
+		
+		alert("array made" + grid.squares[0][x1])
+		
+		alert(!outsideBoard(x1, y1+1, grid))
+		var x, y;
+		//between each check if the piece in way is enemy, in which case allow capturing it
+		
+		//first down, up, right, left
+		for(x = x1, y = y1 + 1; !outsideBoard(x, y, grid) && grid.squares[y][x].piece == null; y++){
+			moveset.push(grid.squares[y][x]);
+		}
+		if(!outsideBoard(x, y, grid) && grid.squares[y][x].piece.side != this.side){
+			moveset.push(grid.squares[y][x]);
+		}
+		
+		for(x = x1, y = y1 - 1; !outsideBoard(x, y, grid) && grid.squares[y][x].piece == null; y--){
+			moveset.push(grid.squares[y][x]);
+		}
+		if(!outsideBoard(x, y, grid) && grid.squares[y][x].piece.side != this.side){
+			moveset.push(grid.squares[y][x]);
+		}
+		
+		for(x = x1 + 1, y = y1; !outsideBoard(x, y, grid) && grid.squares[y][x].piece == null; x++){
+			moveset.push(grid.squares[y][x]);
+		}
+		if(!outsideBoard(x, y, grid) && grid.squares[y][x].piece.side != this.side){
+			moveset.push(grid.squares[y][x]);
+		}
+		
+		for(x = x1 - 1, y = y1; !outsideBoard(x, y, grid) && grid.squares[y][x].piece == null; x--){
+			moveset.push(grid.squares[y][x]);
+		}
+		if(!outsideBoard(x, y, grid) && grid.squares[y][x].piece.side != this.side){
+			moveset.push(grid.squares[y][x]);
+		}
+		
+		//then diagonal, lower right, upper right, lower left, upper left
+		
+		for(x = x1 + 1, y = y1 + 1; !outsideBoard(x, y, grid) && grid.squares[y][x].piece == null; y++, x++){
+			moveset.push(grid.squares[y][x]);
+		}
+		if(!outsideBoard(x, y, grid) && grid.squares[y][x].piece.side != this.side){
+			moveset.push(grid.squares[y][x]);
+		}
+		
+		for(x = x1 + 1, y = y1 - 1; !outsideBoard(x, y, grid) && grid.squares[y][x].piece == null; y--, x++){
+			moveset.push(grid.squares[y][x]);
+		}
+		if(!outsideBoard(x, y, grid) && grid.squares[y][x].piece.side != this.side){
+			moveset.push(grid.squares[y][x]);
+		}
+		
+		for(x = x1 - 1, y = y1 + 1; !outsideBoard(x, y, grid) && grid.squares[y][x].piece == null; y++, x--){
+			moveset.push(grid.squares[y][x]);
+		}
+		if(!outsideBoard(x, y, grid) && grid.squares[y][x].piece.side != this.side){
+			moveset.push(grid.squares[y][x]);
+		}
+		
+		for(x = x1 - 1, y = y1 - 1; !outsideBoard(x, y, grid) && grid.squares[y][x].piece == null; y--, x--){
+			moveset.push(grid.squares[y][x]);
+		}
+		if(!outsideBoard(x, y, grid) && grid.squares[y][x].piece.side != this.side){
+			moveset.push(grid.squares[y][x]);
+		}
+		
+		
+		return moveset;
+	}
+	
 	this.legalMove = function (x1, y1, x2, y2, grid) 
 	{
 		if (containsOwn(x2, y2, this.side, grid)) {
 			return false;
 		}
 		
-		if (x1 == x2 || y1 == y2) { //move like rook
-			if (x1 == x2) {
-				if (y1 > y2) {
-					for (var i = y1 - 1; i > y2; i--) {
-						if (grid.squares[i][x1].piece != null) {
-							return false;
-						}
-					}
-				} else {
-					for (var i = y1 + 1; i < y2; i++) {
-						if (grid.squares[i][x1].piece != null) {
-							return false;
-						}
-					}
-				}
-				return true;
-			}
-			if (y1 == y2) {
-				if (x1 > x2) {
-					for (var i = x1 - 1; i > x2; i--) {
-						if (grid.squares[y1][i].piece != null) {
-							return false;
-						}
-					}
-				} else {
-					for (var i = x1 + 1; i < x2; i++) {
-						if (grid.squares[y1][i].piece != null) {
-							return false;
-						}
-					}
-				}
-				return true;
-			}
-		
-		} else if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) { //move like bishop
-			if (x1 > x2 && y1 > y2) {
-				for (var i = x1 - 1, j = y1 - 1; i > x2; i--, j--) {
-					if (grid.squares[j][i].piece != null) {
-						return false;
-					}
-				}
-			} else if (x1 < x2 && y1 > y2) {
-				for (var i = x1 + 1, j = y1 - 1; i < x2; i++, j--) {
-					if (grid.squares[j][i].piece != null) {
-						return false;
-					}
-				}
-			} else if (x1 > x2 && y1 < y2) {
-				for (var i = x1 - 1, j = y1 + 1; i > x2; i--, j++) {
-					if (grid.squares[j][i].piece != null) {
-						return false;
-					}
-				}
-			} else if (x1 < x2 && y1 < y2) {
-				for (var i = x1 + 1, j = y1 + 1; i < x2; i++, j++) {
-					if (grid.squares[j][i].piece != null) {
-						return false;
-					}
-				}
-			}
+		if(this.moveset(x1, y1, grid).lastIndexOf(grid.squares[y2][x2]) != -1){
 			return true;
-		}
-
-
-		return false;
+		} else return false;
 	}
 
 }
