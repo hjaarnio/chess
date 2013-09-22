638e866d79269ff4705c784f66ca751eb7d388b1
hjaarnio <hjaarnio@gmail.com>
2013-09-22 03:13:07 +0300

    Changed letters to unicode chess charset

--- ui.js
+++ ui.js
@@ -3,7 +3,8 @@ var squareWidth = document.getElementById("dummy").clientWidth / 8,
 	
 var selected = null;
 
-var pieces = [["p", "r", "b", "n", "q", "k"] , ["P", "R", "B", "N", "Q", "K"]];
+//var pieces = [["p", "r", "b", "n", "q", "k"] , ["P", "R", "B", "N", "Q", "K"]];
+var pieces = [["&#9823", "&#9820", "&#9821", "&#9822", "&#9819", "&#9818"] , ["&#9817", "&#9814", "&#9815", "&#9816", "&#9813", "&#9812"]];
 
 function draw(grid){
 	var output = "";
@@ -11,11 +12,11 @@ function draw(grid){
 		for (var j = 0; j < grid.gridHeight; j++){
 			if (grid.squares[i][j].piece != null){
 				if(selected != null && selected.y == i && selected.x == j){
-					output += "<b>";
+					output += "<FONT style='BACKGROUND-COLOR: yellow'>";
 				}
 				output += pieces[grid.squares[i][j].piece.side][grid.squares[i][j].piece.type];
 				if(selected != null && selected.y == i && selected.x == j){
-					output += "</b>";
+					output += "</FONT>";
 				}
 				
 			} else {
