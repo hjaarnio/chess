984c66f3d2654451019304a520263ef6c59ac6eb
hjaarnio <hjaarnio@gmail.com>
2013-10-01 16:06:17 +0300

    Pawn model and basic drawing

--- ui3d.js
+++ ui3d.js
@@ -1,22 +1,26 @@
 var ui = document.getElementById("ui");
-
+alert("blob")
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, ui.offsetWidth/ui.offsetHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(ui.offsetWidth, ui.offsetHeight);
 ui.appendChild(renderer.domElement);
+alert("creating loader")
+var loader = new THREE.JSONLoader();
+loader.load("assets/models/pawn.js", function(geometry){ alert("enter load")
+	var material = new THREE.MeshPhongMaterial({color: 0x0ff090, shading: THREE.SmoothShading});
+	var mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(0, 0, 0);
+	mesh.rotation.set(0, 0, 0);
+	scene.add(mesh);
+    }
+);
+var light = new THREE.PointLight( 0xffffff, 1, 100 ); light.position.set( 5, 5, 5 ); scene.add( light );
 
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-
-scene.add(cube);
 camera.position.z = 5;
 var render = function () { 
 	requestAnimationFrame(render);
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
 	renderer.render(scene, camera);
 };
 render();
